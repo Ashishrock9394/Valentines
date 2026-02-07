2a24ab30fded74ce613b3f952dca62899d2db433
Ashishrock9394 <ashishkumar9394@gmail.com>
2026-02-07 21:05:42 +0530

    update

--- src/utils/getTodayPage.js
+++ src/utils/getTodayPage.js
@@ -1,5 +1,9 @@
 export const getTodayPage = () => {
-  const today = new Date().getDate();
+  const now = new Date();
+  const utc = now.getTime() + now.getTimezoneOffset() * 60000;
+  const istOffset = 5.5 * 60 * 60 * 1000; // IST = UTC+5:30
+  const istDate = new Date(utc + istOffset);
+  const today = istDate.getDate();
 
   const map = {
     7: "rose",
@@ -12,6 +16,5 @@ export const getTodayPage = () => {
     14: "valentine",
   };
 
-  // return "valentine";
   return map[today] || "valentine";
 };
--- src/utils/isUnlocked.js
+++ src/utils/isUnlocked.js
@@ -1,5 +1,9 @@
 export const isUnlocked = (dayNumber) => {
-  const today = new Date().getDate();
-  // return true;
+  const now = new Date();
+  const utc = now.getTime() + now.getTimezoneOffset() * 60000;
+  const istOffset = 5.5 * 60 * 60 * 1000; // IST = UTC+5:30
+  const istDate = new Date(utc + istOffset);
+  const today = istDate.getDate();
+
   return today >= dayNumber;
 };
